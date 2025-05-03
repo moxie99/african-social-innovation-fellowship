ae7ee45cd2992bddba9d621eec40969dae214eeb
Adeolu David <adeolusegun99@gmail.com>
2025-05-03 17:55:21 +0100

    feat: fixed tje dashboard card shown

--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -1,4 +1,4 @@
-import { TrendingDownIcon, TrendingUpIcon } from 'lucide-react'
+import { TrendingUpIcon } from 'lucide-react'
 
 import { Badge } from '@/components/ui/badge'
 import {
@@ -14,9 +14,9 @@ export function SectionCards() {
     <div className='*:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4 grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card lg:px-6'>
       <Card className='@container/card'>
         <CardHeader className='relative'>
-          <CardDescription>Total Revenue</CardDescription>
+          <CardDescription>Course 1</CardDescription>
           <CardTitle className='@[250px]/card:text-3xl text-2xl font-semibold tabular-nums'>
-            $1,250.00
+            Startup Founders
           </CardTitle>
           <div className='absolute right-4 top-4'>
             <Badge variant='outline' className='flex gap-1 rounded-lg text-xs'>
@@ -30,11 +30,11 @@ export function SectionCards() {
             Trending up this month <TrendingUpIcon className='size-4' />
           </div>
           <div className='text-muted-foreground'>
-            Visitors for the last 6 months
+            Please complete in the next two months
           </div>
         </CardFooter>
       </Card>
-      <Card className='@container/card'>
+      {/* <Card className='@container/card'>
         <CardHeader className='relative'>
           <CardDescription>New Customers</CardDescription>
           <CardTitle className='@[250px]/card:text-3xl text-2xl font-semibold tabular-nums'>
@@ -95,7 +95,7 @@ export function SectionCards() {
           </div>
           <div className='text-muted-foreground'>Meets growth projections</div>
         </CardFooter>
-      </Card>
+      </Card> */}
     </div>
   )
 }
